6c102caede588ff7dbe4678ac7697e453ec33d70
victor-flindt <vicorflindt@gmail.com>
2022-01-12 22:40:58 +0100

    added changes to test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -9,51 +9,46 @@ from torchvision.datasets import MNIST
 # module, this is done since the self made dataloader is to time consuming and is not the point of the #
 # exercise in the first place.                                                                         #
 ########################################################################################################
-
-
-def test_train_len():
+def get_train():
     mnist_transform = transforms.Compose([transforms.ToTensor()])
-    N_train = 60000
     dataset_path = 'datasets'
     train_dataset = MNIST(dataset_path,
-                          transform=mnist_transform,
-                          train=True,
-                          download=True)
+                    transform=mnist_transform,
+                    train=True,
+                    download=True)
+    return train_dataset
+
+def get_test():
+    mnist_transform = transforms.Compose([transforms.ToTensor()])
+    dataset_path = 'datasets'
+    test_dataset = MNIST(dataset_path,
+                        transform=mnist_transform,
+                        train=False,
+                        download=True)
+    return test_dataset
+
+def test_train_len():
+    train_dataset=get_train()
+    N_train = 60000
     assert len(train_dataset) == N_train
 
 
 def test_len_answ():
-    mnist_transform = transforms.Compose([transforms.ToTensor()])
+    test_dataset=get_test()
     N_test = 10000
-    dataset_path = 'datasets'
-    test_dataset = MNIST(dataset_path,
-                         transform=mnist_transform,
-                         train=False,
-                         download=True)
-
     assert len(test_dataset) == N_test
 
 
 def test_dimnesion():
     dimension = torch.Size([28, 28])
-    dataset_path = 'datasets'
-    mnist_transform = transforms.Compose([transforms.ToTensor()])
-    test_dataset = MNIST(dataset_path,
-                         transform=mnist_transform,
-                         train=False,
-                         download=True)
+    test_dataset=get_test()
     for i in range(len(test_dataset)):
         assert test_dataset.data[i].shape == dimension,f"datapoint {i} does not corrospond with the input dimesion {dimension}"
 
 
 def test_label_rep():
     labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    mnist_transform = transforms.Compose([transforms.ToTensor()])
-    dataset_path = 'datasets'
-    train_dataset = MNIST(dataset_path,
-                          transform=mnist_transform,
-                          train=True,
-                          download=True)
+    train_dataset=get_train()
     ## input all the expected labels for the data, will throw error if a label in the labels array is not present in the dataset
     ## this array can be made into a tuble if string labels are required. 
     for index, value in enumerate(labels):
